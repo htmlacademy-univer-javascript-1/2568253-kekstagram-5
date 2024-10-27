882e3ee09ccdbcee806e0096fb5e3d6bc873d5f8
Даша <sadkova24092002@gmail.com>
2024-10-27 17:18:44 +0500

    попытка 2

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,14 @@
-
 function lineLength(string, maxLength) {
-  if (string.length <= maxLength) { return true; }
-  return false;
+  return string.length <= maxLength;
 }
 
 // eslint-disable-next-line no-console
 console.log(lineLength('проверяемая строка', 20));
-
-
-
 function checkingForPalindrome(string) {
-  let cleanString = string.toLowerCase();
-  if (cleanString.slice(0, Math.ceil(cleanString.length / 2)) === cleanString.slice(-Math.ceil(cleanString.length / 2)).split('').reverse().join('')) {
-    return true;
-  }
-  return false;
+  const cleanString = string.toLowerCase();
+  return cleanString.slice(0, Math.ceil(cleanString.length / 2)) ===
+         cleanString.slice(-Math.ceil(cleanString.length / 2)).split('').reverse().join('');
 }
+
 // eslint-disable-next-line no-console
 console.log(checkingForPalindrome('топот'));
-
